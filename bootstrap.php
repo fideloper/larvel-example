8f0d50021e5a68a1c485e58361600805c81c07fd
fideloper <fideloper@gmail.com>
2014-10-29 17:42:26 -0400

    ♥️

---
+++ bootstrap.php
@@ -0,0 +1,27 @@
+<?php
+
+require __DIR__.'/vendor/autoload.php';
+
+date_default_timezone_set('UTC');
+
+
+/**
+ * Gestation before becoming a beautiful butterfly
+ */
+$app = new \Larvel™\🐛;
+
+
+
+/**
+ * Set Routes
+ */
+$router = $app['🏃'];
+
+$router->get('/', function($request)
+{
+    return '🎆';
+});
+
+
+
+return $app;
